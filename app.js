61bdf342cc762c2d7b0ce047384165eabeca6342
scott <psiauto@gmail.com>
2022-04-29 12:15:43 -0600

    mobil dropdown nav menu

---
+++ app.js
@@ -0,0 +1,14 @@
+const navMenu = document.getElementById('menu-open')
+const navClose = document.getElementById('menu-close')
+const navDropDownMenu = document.getElementById('drop-down-menu')
+let showMenu = false
+function toggleNavMenu () {
+  showMenu = !showMenu
+  if (showMenu) {
+    navDropDownMenu.style.display = 'unset'
+  } else {
+    navDropDownMenu.style.display = 'none'
+  }
+}
+navMenu.addEventListener('click', toggleNavMenu)
+navClose.addEventListener('click', toggleNavMenu)

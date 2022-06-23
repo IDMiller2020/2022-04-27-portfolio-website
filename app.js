4d0449f05b903119b5729a870e7c8e1984b11844
scott <psiauto@gmail.com>
2022-06-23 11:59:43 -0600

    Project 1 info modal created.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const navClose = document.getElementById('menu-close')
 const linkClickClose = document.getElementById('nav-list')
 const navDropDownMenu = document.getElementById('drop-down-menu')
 let showMenu = false
+const modal = document.getElementById("project-one-modal");
+const btn = document.getElementById("project-one-info");
+const span = document.getElementsByClassName("close")[0];
 function toggleNavMenu () {
   showMenu = !showMenu
   if (showMenu) {
@@ -11,6 +14,19 @@ function toggleNavMenu () {
     navDropDownMenu.style.display = 'none'
   }
 }
+function showModal() {
+  modal.style.display = 'block'
+}
+function modalClose() {
+  modal.style.display = 'none'
+}
 navMenu.addEventListener('click', toggleNavMenu)
 navClose.addEventListener('click', toggleNavMenu)
-linkClickClose.addEventListener('click', toggleNavMenu)
+linkClickClose.addEventListener('click', toggleNavMenu);
+btn.addEventListener('click', showModal)
+span.addEventListener('click', modalClose)
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
